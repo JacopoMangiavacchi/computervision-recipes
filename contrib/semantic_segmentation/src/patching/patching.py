5519559dd50657378d63a12c732bf541756931af
Jacopo Mangiavacchi <jamangia@microsoft.com>
2021-07-09 14:16:26 +0200

    wip pyramid patching

--- contrib/semantic_segmentation/src/patching/patching.py
+++ contrib/semantic_segmentation/src/patching/patching.py
@@ -70,3 +70,49 @@ def extract_patches(
                     patches.append((patch_image, patch_mask_image))
 
     return patches
+
+def extract_pyramid_patches(
+    image: object,
+    mask: object,
+    classes: List[int],
+    patch_dimension: Tuple[int, int] = (1000, 1000),
+    window_overlap: float = 0.1,
+    threshold: int = 100,
+) -> List[Tuple[object, object]]:
+    """For an input image with a relative mask return
+    a list of all extracted patched images and corresponding mask images
+
+    Parameters
+    ----------
+    image : object
+        Original source image
+    mask : object
+        Original mask image
+    classes : List[int]
+        list of classes to use for the patch image
+    patch_dimension : Tuple(Int, Int)
+        Width and Height of the extracted patch
+    window_overlap : Float
+        increment window by % of patch_dimension
+    threshold : Int
+        minimum number of pixels in patch mask
+
+    Returns
+    -------
+    patch_list : List[Tuple[object, object]]
+        List of all extracted patches and corresponding mask images
+    """
+    patch_images = []
+
+    patches = extract_patches(
+        image=image, 
+        mask=mask,
+        classes=classes,                                                               
+        patch_dimension=patch_dimension, 
+        window_overlap=window_overlap,
+        threshold=threshold)
+
+    patch_images.extend((i.resize(patch_dimension), p.resize(patch_dimension)) for i, p in patches)
+
+    return patch_images
+
