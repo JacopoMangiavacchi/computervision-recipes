664a24fd332bee9086999e60265049c58a3dc2be
Jacopo Mangiavacchi <jamangia@microsoft.com>
2021-07-10 13:51:59 +0200

    WIP adding extract script

--- contrib/semantic_segmentation/src/extract_patches.py
+++ contrib/semantic_segmentation/src/extract_patches.py
@@ -20,9 +20,6 @@ logging.basicConfig(level=logging.INFO)
 
 def save_patch_and_mask(
     patch_images,
-    gdt,
-    cat,
-    typ,
     name,
     output_images_dir,
     patch_folder,
@@ -32,12 +29,12 @@ def save_patch_and_mask(
     for i in range(len(patch_images)):
         # saving patch
         patch_path = os.path.join(output_images_dir, data_folder, patch_folder)
-        patch_name = "_".join([gdt, cat, typ, name.split(".")[0], str(i)]) + ".png"
+        patch_name = "_".join([name.split(".")[0], str(i)]) + ".png"
         patch_images[i][0].save(patch_path + "/" + patch_name)
 
         # saving mask
         mask_path = os.path.join(output_images_dir, data_folder, mask_folder)
-        mask_name = "_".join([gdt, cat, typ, name.split(".")[0], str(i)]) + ".png"
+        mask_name = "_".join([name.split(".")[0], str(i)]) + ".png"
         patch_images[i][1].save(mask_path + "/" + mask_name)
 
 
@@ -53,60 +50,26 @@ def process_file(
     mask_folder,
 ):
     index, _ = tuple_value
-    image_info = coco_json["images"][index]
 
     if index >= start and (end == -1 or index <= end):
-        _, gdt, cat, typ, name = image_info["file_name"].split("/")
-        image_annotations = annotations_by_image_id[image_info["id"]]
-        image_filepath = join(input_images_dir, image_info["file_name"])
+        image_filepath = join(input_images_dir, all_images[index])
+        mask_filepath = join(input_masks_dir, all_masks[index])
         image = Image.open(image_filepath)
+        mask = Image.open(mask_filepath)
 
-        if sub_annotation.lower() in ["true", "1", "t", "y", "yes"]:
-            if pyramid.lower() in ["true", "1", "t", "y", "yes"]:
-                patch_images = []
-
-                for pyramid_patch in pyramid_patches:
-                    ext_patch = extract_windowed_patches_and_mask_images_sub_annotation(
-                        image=image,
-                        annotations=image_annotations,
-                        classes=classes,
-                        patch_dimension=(pyramid_patch, pyramid_patch),
-                        window_overlap=float(window_overlap),
-                        threshold=int(threshold),
-                    )
-
-                    patch_images.extend(
-                        (
-                            x.resize((patch_width, patch_height)),
-                            y.resize((patch_width, patch_height)),
-                        )
-                        for x, y in ext_patch
-                    )
-            else:
-                patch_images = extract_windowed_patches_and_mask_images_sub_annotation(
-                    image=image,
-                    annotations=image_annotations,
-                    classes=classes,
-                    patch_dimension=(patch_width, patch_height),
-                    window_overlap=float(window_overlap),
-                    threshold=int(threshold),
-                )
-        else:
-            patch_images = extract_windowed_patches_and_mask_images(
-                image=image,
-                annotations=image_annotations,
-                classes=classes,
-                patch_dimension=(patch_width, patch_height),
-                window_overlap=float(window_overlap),
-            )
-
+        patch_images = extract_pyramid_patches(
+            image=image, 
+            mask=mask,
+            classes=classes,                                                               
+            patch_dimension=(patch_width, patch_height), 
+            pyramid_dimensions=pyramid_patches,
+            window_overlap=float(window_overlap),
+            threshold=int(threshold))
+    
         if index in train_indexes:
             save_patch_and_mask(
                 patch_images,
-                gdt,
-                cat,
-                typ,
-                name,
+                image_filepath,
                 output_images_dir,
                 patch_folder,
                 mask_folder,
@@ -115,10 +78,7 @@ def process_file(
         if index in val_indexes:
             save_patch_and_mask(
                 patch_images,
-                gdt,
-                cat,
-                typ,
-                name,
+                image_filepath,
                 output_images_dir,
                 patch_folder,
                 mask_folder,
@@ -127,10 +87,7 @@ def process_file(
         if index in test_indexes:
             save_patch_and_mask(
                 patch_images,
-                gdt,
-                cat,
-                typ,
-                name,
+                image_filepath,
                 output_images_dir,
                 patch_folder,
                 mask_folder,
@@ -183,6 +140,7 @@ if __name__ == "__main__":
     np.random.seed(seed)
     
     all_images = list(glob.iglob(f'{input_images_dir}/*.jpg'))
+    all_masks = list(glob.iglob(f'{input_masks_dir}/*.png'))
     all_images_indexes = list(range(len(all_images)))
 
     np.random.shuffle(all_images_indexes)
@@ -195,37 +153,37 @@ if __name__ == "__main__":
         ],
     )
 
-    # for data_folder in ["train", "validation", "test"]:
-    #     patch_path = os.path.join(output_images_dir, data_folder, patch_folder)
-    #     os.makedirs(patch_path, exist_ok=True)
-    #     print("Directory '%s' created" % patch_path)
+    for data_folder in ["train", "validation", "test"]:
+        patch_path = os.path.join(output_images_dir, data_folder, patch_folder)
+        os.makedirs(patch_path, exist_ok=True)
+        print("Directory '%s' created" % patch_path)
 
-    #     mask_path = os.path.join(output_images_dir, data_folder, mask_folder)
-    #     os.makedirs(mask_path, exist_ok=True)
-    #     print("Directory '%s' created" % mask_path)
+        mask_path = os.path.join(output_images_dir, data_folder, mask_folder)
+        os.makedirs(mask_path, exist_ok=True)
+        print("Directory '%s' created" % mask_path)
 
-    # print(f"Running in parallel on {os.cpu_count()} processes")
-    # pool = multiprocessing.Pool()
-    # images_processed = list(
-    #     pool.imap(
-    #         partial(
-    #             process_file,
-    #             start=start,
-    #             end=end,
-    #             train_indexes=train_indexes,
-    #             val_indexes=val_indexes,
-    #             test_indexes=test_indexes,
-    #             output_images_dir=output_images_dir,
-    #             patch_folder=patch_folder,
-    #             mask_folder=mask_folder,
-    #         ),
-    #         enumerate(range(len(coco_json["images"]))),
-    #     )
-    # )
-    # pool.close()
-    # pool.join()
+    print(f"Running in parallel on {os.cpu_count()} processes")
+    pool = multiprocessing.Pool()
+    images_processed = list(
+        pool.imap(
+            partial(
+                process_file,
+                start=start,
+                end=end,
+                train_indexes=train_indexes,
+                val_indexes=val_indexes,
+                test_indexes=test_indexes,
+                output_images_dir=output_images_dir,
+                patch_folder=patch_folder,
+                mask_folder=mask_folder,
+            ),
+            enumerate(range(len(all_images))),
+        )
+    )
+    pool.close()
+    pool.join()
 
-    # print(f"Processed {sum(images_processed)} images")
-    # print(
-    #     f"Train: {len(train_indexes)} Validation: {len(val_indexes)} Test: {len(test_indexes)}"
-    # )
+    print(f"Processed {sum(images_processed)} images")
+    print(
+        f"Train: {len(train_indexes)} Validation: {len(val_indexes)} Test: {len(test_indexes)}"
+    )
